3e1d9f1475cfd36eabafbd13f4597dad25d021d1
Ahmedkhan <sahmedkh@jcp.com>
2019-08-17 23:02:18 +0530

    Added fullfilment message

--- src/main/java/com/assistant/expenseManager/controllers/AssistantController.java
+++ src/main/java/com/assistant/expenseManager/controllers/AssistantController.java
@@ -1,8 +1,10 @@
 package com.assistant.expenseManager.controllers;
 
 import com.assistant.expenseManager.models.FF;
+import com.assistant.expenseManager.models.FulfillmentMessage;
 import com.assistant.expenseManager.models.FulfillmentRequest;
 import com.assistant.expenseManager.models.FulfillmentResponse;
+import com.assistant.expenseManager.models.TextMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @Slf4j
 @RestController
@@ -31,9 +35,20 @@ public class AssistantController {
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<FulfillmentResponse> assistantRequest(@RequestBody FulfillmentRequest fulfillmentRequest) {
 
-        String name = fulfillmentRequest.getQueryResult().getIntent().getName();
+        String name = fulfillmentRequest.getQueryResult().getIntent().getDisplayName();
         FF ff = new FF(fulfillmentRequest);
         FulfillmentResponse ffR = ff.getResponse();
+
+        TextMessage textMessage = TextMessage.builder()
+                .text(Arrays.asList("", ""))
+                .build();
+
+        ffR.setFulfillmentMessages(Arrays.asList(
+                FulfillmentMessage.builder().text(TextMessage.builder()
+                        .text(Arrays.asList("Hello your intent is" + name, "Any thing else"))
+                        .build()).build())
+        );
+
         ffR.setFulfillmentText(" Hello your intent is " + name);
         return ResponseEntity.ok(ffR);
     }
